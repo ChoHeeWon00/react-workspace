0e35a93c91d4c05e9eaf7335d1f0439056310966
choheewon <choheewon@hanmail.net>
2026-01-05 13:20:38 +0900

    2일차

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import StateTest01 from './StateTest01';
 import Ex01Con from './containers/Ex01Con';
 import Quiz01Con from './containers/Quiz01Con';
+import Quiz02Con from './containers/Quiz02Con';
 function App() {
   return (<>
+  <Quiz02Con />
+  <hr></hr>
   <Quiz01Con />
 
   <hr></hr>
--- ex02/src/components/Quiz02Com.js
+++ ex02/src/components/Quiz02Com.js
@@ -1,6 +1,18 @@
-function Quiz02Com(  ){
+function Quiz02Com( props ){
+    console.log( props )
+    const {msg:mm, onIn, onOut } = props
+    let message = "";
+    if(mm === "")
+        message = <b>아무값 없음</b>;
+    else
+        message = mm;
     return (<>
-       quiz 02 com
+        삼항연산자 : { (mm === "")? <b>없음</b> : mm }<br></br>
+       msg : {mm}<br></br>
+       message : {message}
+       <br></br>
+       <button onClick={props.onIn}>IN</button>
+       <button onClick={onOut}>OUT</button>
     </>)
 }
 export default Quiz02Com;

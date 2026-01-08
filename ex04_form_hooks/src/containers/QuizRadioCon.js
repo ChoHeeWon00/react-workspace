6f6949c9d110af759666f0887342199b9334fe18
choheewon <choheewon@hanmail.net>
2026-01-08 17:19:39 +0900

    4일차

---
+++ ex04_form_hooks/src/containers/QuizRadioCon.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import QuizRadioCom from "../components/QuizRadioCom";
+const initalRadio = [
+    {key : 4, value : "10대", name : "age", isChecked : true},
+    {key : 5, value : "20대", name : "age", isChecked : false},
+    {key : 6, value : "30대", name : "age", isChecked : false},
+]
+const QuizRadioCon = () => {
+    const [age, setAge ] = useState( initalRadio )
+    const onChange = ( e ) => {}
+    return (<><QuizRadioCom age={age} onChange={onChange}/></>)
+}
+export default QuizRadioCon;

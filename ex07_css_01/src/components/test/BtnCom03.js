75f1306840c8aa557bc936a271cb1f669b84177f
choheewon <choheewon@hanmail.net>
2026-01-15 12:28:57 +0900

    8일차

--- ex07_css_01/src/components/test/BtnCom03.js
+++ ex07_css_01/src/components/test/BtnCom03.js
@@ -6,10 +6,27 @@ export default styled.button`
   background-color : blue; color : white;
   border : none; border-radius : 5px;
   cursor : pointer;
+  ${ props => css`
+        ${props.width && `width : ${props.width};` }
+        ${props.background && `
+            background-color : rgba(${props.background[0]}, 
+                                    ${props.background[1] });
+        ` }
+  `}
+  /*
     ${ props => props.width && css`
         width : ${props.width};
     `}
+    ${ props => props.background && css`
+        background-color : rgba(${props.background[0]}, 
+                                    ${props.background[1] });
+    `}
+    */
   &:hover {
     background-color : darkblue;
+    ${ props => props.background && css`
+        background-color : rgba(${props.background[0]}, 
+                                    ${props.background[1]+0.4 });
+    `}
   }
 `;
--- ex07_css_01/src/components/test/TestCom03.js
+++ ex07_css_01/src/components/test/TestCom03.js
@@ -2,7 +2,8 @@ import StyleButton from "./BtnCom03";
 const Test03 = () => {
     return (<>
         <StyleButton>Test03</StyleButton>
-        <StyleButton width="100px">Test03</StyleButton>
+        <StyleButton width="100px" 
+                    background={["255,0,0",0.5]}>로그인</StyleButton>
     </>)
 }
 export default Test03;
